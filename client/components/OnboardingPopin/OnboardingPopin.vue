6f6bacb1fd7dd86ec8eb0f44dc2019505afc677b
leach <lea.charrier@hetic.net>
2021-05-15 01:02:32 +0200

    add onboarding popin

---
+++ client/components/OnboardingPopin/OnboardingPopin.vue
@@ -0,0 +1,80 @@
+<template>
+  <div v-show="isOpen" class="bgPopin">
+    <div class="popin">
+      <div class="popin-content">
+        <h2 class="title">
+          Welcome to Basis !
+        </h2>
+        <p class="txt_caption description">
+          Basis is a <span>design data platform</span> that focuses on helping you reach convergence inside <br> your <span>design system</span>, both in <span>design and code</span>.
+        </p>
+
+        <VueSlickCarousel v-bind="settings" class="slider">
+          <div class="slider-item">
+            <p class="text">
+              Gain velocity on each step of the project
+            </p>
+            <img src="~assets/img/signup.svg" class="img">
+          </div>
+          <div class="slider-item">
+            <p class="text">
+              Master your design system from A to Z
+            </p>
+            <img src="~assets/img/atoz.svg" class="img">
+          </div>
+          <div class="slider-item">
+            <p class="text">
+              Reconcile designers and developers
+            </p>
+            <img src="~assets/img/sign.svg" class="img">
+          </div>
+        </VueSlickCarousel>
+      </div>
+      <div class="popin-btn">
+        <button class="btn is-blue" @click="closePopin()">
+          <p>
+            Get started
+          </p>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+// optional style for arrows & dots
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+
+export default {
+  name: 'OnboardingPopin',
+  components: {
+    VueSlickCarousel
+  },
+  data () {
+    return {
+      settings: {
+        arrows: false,
+        dots: true,
+        dotsClass: 'slick-dots dot-img',
+        edgeFriction: 0.35,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      },
+      isOpen: true
+    }
+  },
+  methods: {
+    closePopin () {
+      this.isOpen = false
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "OnboardingPopin";
+</style>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -10,6 +10,8 @@
         />
       </div>
     </div>
+
+    <OnboardingPopin />
   </div>
 </template>
 
@@ -18,13 +20,15 @@ import { mapGetters } from 'vuex'
 import Header from '~/components/Header/Header'
 import Aside from '~/components/Aside/Aside'
 import BlockTitleDesc from '~/components/BlockTitleDesc/BlockTitleDesc'
+import OnboardingPopin from '~/components/OnboardingPopin/OnboardingPopin'
 
 export default {
   name: 'Home',
   components: {
     Header,
     Aside,
-    BlockTitleDesc
+    BlockTitleDesc,
+    OnboardingPopin
   },
   computed: {
     ...mapGetters({
