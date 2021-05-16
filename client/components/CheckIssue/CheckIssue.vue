43f01e9b98e5c30e410936ada0edb782a7eb890f
leach <lea.charrier@hetic.net>
2021-05-17 00:04:46 +0200

    check issue

---
+++ client/components/CheckIssue/CheckIssue.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="checkIssue">
+    <form class="content">
+      <input type="checkbox" id="checkbox" v-model="checked">
+      <label for="checkbox" class="txt_body title">Show resolved issues</label>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CheckIssue'
+}
+</script>
+
+<style scoped lang="scss">
+@import "CheckIssue";
+</style>
