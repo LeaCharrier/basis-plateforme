b3899bdb1a1b4940e3e2ba63b9fb408147057f73
Adrien <adrien.ww@gmail.com>
2021-05-25 21:57:42 +0200

    attach used style id to every filled elements

--- server/utils/FileFormat.js
+++ server/utils/FileFormat.js
@@ -1,10 +1,11 @@
-import {rgbToHex} from "./ColorConvertor.js";
+import {
+    rgbToHex
+} from "./ColorConvertor.js";
 import slugify from 'slugify'
 
 export function fileFormat(file) {
-    const items = {
-        colors: []
-    }
+    const items = [];
+    let i = 0;
 
     file.document.children
         .filter(child => child.type === 'CANVAS')
@@ -22,35 +23,47 @@ export function fileFormat(file) {
                         colors: []
                     }
 
-                    return child.children
-                        .filter(child => child.fills.length)
-                        .map(shade => {
-                            if (shade.fills[0] && shade.fills[0].color) {
+                    let rec = obj => {
+                        [...obj].forEach(o => {
+                            if(o.children) {
+                                rec(o.children);
+                            } else if(o.fills.length && o.fills[0] && o.fills[0].color) {
+
+                                // Store used style id
+                                let style;
+                                o.styles && o.styles.fill ? style = o.styles.fill : style = undefined;
+
                                 const {
                                     r,
                                     g,
                                     b,
                                     a
-                                } = shade.fills[0].color
-
+                                } = o.fills[0].color
+                                
                                 const color = {
-                                    name: shade.name,
-                                    type: shade.type,
+                                    name: o.name,
+                                    type: o.type,
+                                    style: style,
                                     r: r * 255,
                                     g: g * 255,
                                     b: b * 255,
                                     a,
                                     hex: rgbToHex(r * 255, g * 255, b * 255),
-                                    fillType: shade.fills[0].type
+                                    fillType: o.fills[0].type
                                 }
 
                                 items[`${slug}-${id}`].colors.push(color)
-                                items.colors.push(color)
+                                items.push(color)
+                                
                             }
-                        })
+                        });
+                    }
+
+                    rec(child.children);
+
                 })
         })
-
+    
     return items
 }
 
@@ -65,6 +78,7 @@ export function jsonFileFormat(json) {
             const data = json[k][s]
 
             if (data.type) {
+                console.log(data.type);
                 switch (data.type) {
                     case 'color':
                         if (!res.colors)
