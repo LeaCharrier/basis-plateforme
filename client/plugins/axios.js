a53b73dc44558ce495003c3a98e471db38b7d72d
Samy Yahia <syahia93250@yahoo.com>
2021-06-17 16:28:05 +0200

    Fix api

--- client/plugins/axios.js
+++ client/plugins/axios.js
@@ -2,7 +2,7 @@ export default function ({ $axios }, inject) {
   const api = $axios.create()
 
   // Set baseURL to something different
-  api.setBaseURL('http://basis-api.herokuapp.com/api/')
+  api.setBaseURL('https://basis-api.herokuapp.com/api/')
 
   // Inject to context as $api
   inject('api', api)
