c4cbdede735e8fcdbfb3b4f5f8b85a47cb230b48
Samy Yahia <syahia93250@yahoo.com>
2021-06-18 22:47:09 +0200

    Up server CORS

--- server/server.js
+++ server/server.js
@@ -29,6 +29,23 @@ function haltOnTimedout(req, res, next){
 
 app.use(cors());
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+};
+
+
+app.use(allowCrossDomain);
+
 // routes
 app.use('/api/figma', FigmaRoutes);
 app.use('/api/issues', IssueRoutes);
