40a112794bc85077fd432d1a69d2c55b7213d072
Samy Yahia <syahia93250@yahoo.com>
2021-06-19 01:32:00 +0200

    Update

--- client/plugins/axios.js
+++ client/plugins/axios.js
@@ -2,8 +2,8 @@ export default function ({ $axios }, inject) {
   const api = $axios.create()
 
   // Set baseURL to something different
-  // api.setBaseURL('https://basis-api.herokuapp.com/api/')
-  api.setBaseURL('http://localhost:5000/api/')
+  api.setBaseURL('https://basis-api.herokuapp.com/api/')
+  // api.setBaseURL('http://localhost:5000/api/')
 
   api.defaults.headers.post['Access-Control-Allow-Origin'] = '*'
 
