a32588861358003456462fba36139ced0733cf96
leach <lea.charrier@hetic.net>
2021-06-18 11:04:54 +0200

    add resize window

---
+++ client/components/ErrorMessage/ErrorMessage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="errorMessage">
+      <h1 class="title">This page requires a larger screen</h1>
+      <p class="desc">It looks like your browser is not open full screen or your screen is not large enough, please make your browser window larger or try on a larger screen</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ErrorMessage'
+}
+</script>
+
+<style scoped lang="scss">
+@import "ErrorMessage";
+</style>
