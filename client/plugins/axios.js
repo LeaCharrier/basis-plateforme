a7160fa5d8b3fec4691fda71a8e87d0aad6266ef
Samy Yahia <syahia93250@yahoo.com>
2021-06-21 10:10:59 +0200

    Update Api url

--- client/plugins/axios.js
+++ client/plugins/axios.js
@@ -2,7 +2,8 @@ export default function ({ $axios }, inject) {
   const api = $axios.create()
 
   // Set baseURL to something different
-  api.setBaseURL('http://localhost:5000/api/')
+  api.setBaseURL('https://basis-api.herokuapp.com/api/')
+  // api.setBaseURL('http://localhost:5000/api/')
 
   // Inject to context as $api
   inject('api', api)
